853879d4ccd8a21adfbb18c12ff393c22293db5e
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-08-03 10:05:12 +0300

    fixing loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export default function Loading() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center gap-4">
             <DotLottieReact
-                src="public/loading.json"
+                src="/loading.json"
                 loop
                 autoplay
                 style={{ width: 240, height: 240 }}
