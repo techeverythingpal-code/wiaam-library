3df66e11c4d7c5162e68e17ffd0d80cecdf663de
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-08-06 11:51:49 +0300

    debugging

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,15 @@
 
 import { DotLottieReact, setWasmUrl } from '@lottiefiles/dotlottie-react';
 
+console.log('[DEBUG] loading.tsx module loaded');
+
 setWasmUrl('/dotlottie-player.wasm');
 
+console.log('[DEBUG] setWasmUrl called');
+
 export default function Loading() {
+    console.log('[DEBUG] Loading component function called');
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-center gap-4">
             <DotLottieReact
@@ -12,6 +18,8 @@ export default function Loading() {
                 loop
                 autoplay
                 style={{ width: 240, height: 240 }}
+                onLoad={() => console.log('[DEBUG] animation onLoad fired')}
+                onLoadError={(e: any) => console.log('[DEBUG] animation onLoadError:', e)}
             />
             <p className="text-gray-500 text-lg">Loading books...</p>
         </div>
