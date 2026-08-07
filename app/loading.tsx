59d395c7ab46d6c909d2df0234ae6e782c092e18
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-08-07 12:15:14 +0300

    ok

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center gap-4">
             <div className="loader" />
-            <p className="text-gray-500 text-lg">Loading books...</p>
+            
         </div>
     );
 }
