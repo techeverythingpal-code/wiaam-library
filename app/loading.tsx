c4e3ec61ef0629cae968571bb920ecf1bff9301d
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-08-07 11:53:18 +0300

    animite bounce

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,31 +1,7 @@
-'use client';
-
-import { DotLottieReact, setWasmUrl } from '@lottiefiles/dotlottie-react';
-
-console.log('[DEBUG] loading.tsx module loaded');
-
-setWasmUrl('/dotlottie-player.wasm');
-
-console.log('[DEBUG] setWasmUrl called');
-
 export default function Loading() {
-    console.log('[DEBUG] Loading component function called');
-
     return (
         <div className="min-h-screen flex flex-col items-center justify-center gap-4">
-            <DotLottieReact
-                src="/loading.json"
-                loop
-                autoplay
-                style={{ width: 240, height: 240 }}
-                dotLottieRefCallback={(dotLottie) => {
-                    console.log('[DEBUG] dotLottieRefCallback fired, instance:', dotLottie);
-                    if (dotLottie) {
-                        dotLottie.addEventListener('load', () => console.log('[DEBUG] animation load event'));
-                        dotLottie.addEventListener('loadError', (e: any) => console.log('[DEBUG] animation loadError event:', e));
-                    }
-                }}
-            />
+            <div className="text-6xl animate-bounce">📚</div>
             <p className="text-gray-500 text-lg">Loading books...</p>
         </div>
     );
